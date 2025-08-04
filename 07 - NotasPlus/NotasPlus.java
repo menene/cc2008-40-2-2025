c5d888298049e139ebeb893d2eac4159f9f4fb1c
menene <efmarroquinr@gmail.com>
2025-08-04 12:34:54 -0600

    notas plus

---
+++ 07 - NotasPlus/NotasPlus.java
@@ -0,0 +1,28 @@
+public class NotasPlus {
+    private Estudiante[] estudiantes;
+    private Curso[] cursos;
+    private double[][] calificaciones; // [estudiante][curso]
+
+    public NotasPlus()
+    {
+    }
+
+    public void iniciar()
+    {
+    }
+
+    public double calcularPromedioGeneral() 
+    {
+        return 0;
+    }
+
+    public double calcularPromedioEstudiante(int indiceEstudiante) 
+    {
+        return 0;
+    }
+
+    public double calcularPromedioCurso(int indiceCurso) 
+    {
+        return 0;
+    }
+}
